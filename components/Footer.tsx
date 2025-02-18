85781ffbe097076f3671b6cd17899438346f096f
Mel Ivan Abolencia <abolencia.iboardliving@gmail.com>
2025-02-18 18:15:46 +0800

    Replace images with React Icons in Footer component for improved icon representation

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,13 @@
-import Image from "next/image";
+import { VscError, VscGitCommit, VscGithub, VscWarning } from "react-icons/vsc";
 
 export default function Footer() { 
     return (
-        <div>
-            <div><Image src='' alt="branchName"/></div>
-            <div><Image src='' alt="errors"/> <Image src='' alt="warnings"/></div>
-            <div><Image src='' alt="nextJSIcon"/><p>Powered By Next.js</p></div>
-            <div><Image src='' alt="doubleCheck"/> <p>Prettier</p> </div>
-            <div><Image src='' alt="notifs"/></div>
+        <div className="flex flex-row  h-fit bg-dark3 text-defaultFont">
+            <VscGitCommit size={20} />  
+            <VscGithub size={20} />
+            <VscError   size={20} />
+            <VscWarning size={20} />
+            <VscGitCommit size={20} />  
         </div>
     )
  }
